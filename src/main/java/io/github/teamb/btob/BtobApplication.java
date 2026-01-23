5e29da169ef3d9bde80075ba2cf7123a3394fdda
suhyeonjo-71 <suhyeonjo71@gmail.com>
2026-01-23 10:53:02 +0900

    First commit

---
+++ src/main/java/io/github/teamb/btob/BtobApplication.java
@@ -0,0 +1,13 @@
+package io.github.teamb.btob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtobApplication.class, args);
+	}
+
+}
